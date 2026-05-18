94cc49795b5270e7f72f44d11b5cf647c8d4218b
Diego Silva <diegossilva2008@gmail.com>
2026-05-18 17:48:57 -0300

    feat: add security infrastructure

---
+++ backend/src/test/java/br/com/pharos/PharosApplicationTests.java
@@ -0,0 +1,15 @@
+package br.com.pharos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class PharosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
